42f82aadd6d6bfe9820806646b7509b1d12c1db8
Radu M <root@radu.sh>
2019-09-17 17:58:59 +0300

    Download files and add tests
    Signed-off-by: Radu M <root@radu.sh>

---
+++ src/configurator.ts
@@ -0,0 +1,96 @@
+import * as core from '@actions/core';
+import * as tc from '@actions/tool-cache';
+import * as exec from '@actions/exec';
+import * as io from '@actions/io';
+import * as path from 'path';
+import * as os from 'os';
+
+export enum ArchiveType {
+    None = "",
+    TarGz = ".tar.gz",
+    Zip = ".zip",
+    SevenZ = ".7z"
+}
+
+export class Configurator {
+    name: string;
+    url: string;
+    pathInArchive: string;
+
+    constructor(name: string, url: string, pathInArchive: string) {
+        this.name = name;
+        this.url = url;
+        this.pathInArchive = pathInArchive;
+    }
+
+    getArchiveType(): ArchiveType {
+        if (this.url.endsWith(ArchiveType.TarGz)) return ArchiveType.TarGz;
+        if (this.url.endsWith(ArchiveType.Zip)) return ArchiveType.Zip;
+        if (this.url.endsWith(ArchiveType.SevenZ)) return ArchiveType.SevenZ;
+
+        return ArchiveType.None;
+    }
+
+    async configure() {
+        console.log(`Downloading tool from ${this.url}...`);
+        let downloadPath: string | null = null;
+        let archivePath: string | null = null;
+        downloadPath = await tc.downloadTool(this.url);
+
+        switch (this.getArchiveType()) {
+            case ArchiveType.None:
+                return this.moveToPath(downloadPath);
+
+            case ArchiveType.TarGz:
+                archivePath = await tc.extractTar(downloadPath);
+                return this.moveToPath(path.join(archivePath, this.pathInArchive));
+
+            case ArchiveType.Zip:
+                archivePath = await tc.extractZip(downloadPath);
+                return this.moveToPath(path.join(archivePath, this.pathInArchive));
+            
+            case ArchiveType.Zip:
+                archivePath = await tc.extract7z(downloadPath);
+                return this.moveToPath(path.join(archivePath, this.pathInArchive));
+        }
+    }
+
+    async moveToPath(downloadPath: string) {
+        let toolPath = binPath();
+        await io.mkdirP(toolPath);
+        await io.mv(downloadPath, path.join(toolPath, this.name));
+        if (process.platform !== 'win32') {
+            await exec.exec("chmod", ["+x", path.join(toolPath, this.name)])
+        }
+
+        core.addPath(toolPath);
+    }
+}
+
+const NameInput: string = "name";
+const URLInput: string = "url";
+const PathInArchiveInput: string = "pathInArchive";
+
+export function getConfig(): Configurator {
+    const n: string = core.getInput(NameInput);
+    const u: string = core.getInput(URLInput);
+    const p: string = core.getInput(PathInArchiveInput);
+
+    return new Configurator(n, u, p);
+}
+
+export function binPath(): string {
+    let baseLocation: string;
+    if (process.platform === 'win32') {
+        // On windows use the USERPROFILE env variable
+        baseLocation = process.env['USERPROFILE'] || 'C:\\';
+    } else {
+        if (process.platform === 'darwin') {
+            baseLocation = '/Users';
+        } else {
+            baseLocation = '/home';
+        }
+    }
+
+    return path.join(baseLocation, os.userInfo().username, "configurator", "bin");
+}
--- test/action.ts
+++ test/action.ts
@@ -1,17 +1,111 @@
+import * as path from 'path';
+const toolDir = path.join(__dirname, 'tmp', 'runner', 'tools');
+const tempDir = path.join(__dirname, 'tmp', 'runner', 'temp');
+const dataDir = path.join(__dirname, 'tmp', 'data');
+
+process.env['RUNNER_TOOL_CACHE'] = toolDir;
+process.env['RUNNER_TEMP'] = tempDir;
+
 import "mocha";
 import { assert } from "chai";
+import * as cfg from '../src/configurator';
+import * as fs from 'fs';
+import * as rimraf from 'rimraf';
 
-const testEnvVars = {
-    INPUT_VERSION: 'v0.5.3',
-};
-
-describe("checking input parsing", function () {
-    beforeEach(() => {
-        for (const key in testEnvVars)
-            process.env[key] = testEnvVars[key as keyof typeof testEnvVars]
+describe("test archive type", () => {
+    it("correctly chooses the NONE archive type", () => {
+        const archiveTypeNoneInput = {
+            INPUT_URL: 'https://github.com/<some-repo>/releases/download/<release>/some-binary',
+        };
+        for (const key in archiveTypeNoneInput)
+            process.env[key] = archiveTypeNoneInput[key]
+        
+        let c = cfg.getConfig();
+        assert.equal(cfg.ArchiveType.None, c.getArchiveType());
     });
 
-    it("correctly parse input", () => {
-        assert.equal('v0.5.3', testEnvVars.INPUT_VERSION);
+    it("correctly chooses the TAR.GZ archive type", () => {
+        const archiveTypeTarInput = {
+            INPUT_URL: 'https://github.com/<some-repo>/releases/download/<release>/some-tar-archive.tar.gz',
+        };
+        for (const key in archiveTypeTarInput)
+            process.env[key] = archiveTypeTarInput[key]
+        
+        let c = cfg.getConfig();
+        assert.equal(cfg.ArchiveType.TarGz, c.getArchiveType());
+    });
+
+    it("correctly chooses the ZIP archive type", () => {
+        const archiveTypeZipInput = {
+            INPUT_URL: 'https://github.com/<some-repo>/releases/download/<release>/some-tar-archive.zip',
+        };
+        for (const key in archiveTypeZipInput)
+            process.env[key] = archiveTypeZipInput[key]
+        
+        let c = cfg.getConfig();
+        assert.equal(cfg.ArchiveType.Zip, c.getArchiveType());
+    });
+
+    it("correctly chooses the 7Z archive type", () => {
+        const archiveTypeZipInput = {
+            INPUT_URL: 'https://github.com/<some-repo>/releases/download/<release>/some-tar-archive.7z',
+        };
+        for (const key in archiveTypeZipInput)
+            process.env[key] = archiveTypeZipInput[key];
+        
+        let c = cfg.getConfig();
+        assert.equal(cfg.ArchiveType.SevenZ, c.getArchiveType());
     });
 });
+
+describe("test download", async () => {
+    after(() => {
+        rimraf.sync(tempDir);
+        rimraf.sync(cfg.binPath());
+    });
+
+    it("correctly downloads plain files", async () => {
+        const input = {
+            INPUT_URL: 'https://github.com/kubernetes-sigs/kind/releases/download/v0.5.1/kind-linux-amd64',
+            INPUT_NAME: 'kind',
+        }
+        for (const key in input)
+            process.env[key] = input[key];
+        
+        let c = cfg.getConfig();
+        await c.configure();
+
+        assert.equal(fs.existsSync(path.join(cfg.binPath(), c.name)), true);
+    });
+
+
+    it("correctly downloads .tar.gz files", async () => {
+        const input = {
+            INPUT_URL: 'https://get.helm.sh/helm-v3.0.0-beta.3-linux-amd64.tar.gz',
+            INPUT_NAME: 'hb3',
+            INPUT_PATHINARCHIVE: 'linux-amd64/helm'
+        }
+        for (const key in input)
+            process.env[key] = input[key];
+        
+        let c = cfg.getConfig();
+        await c.configure();
+
+        assert.equal(fs.existsSync(path.join(cfg.binPath(), c.name)), true);
+    });
+
+    it("correctly downloads .zip files", async () => {
+        const input = {
+            INPUT_URL: 'https://get.helm.sh/helm-v3.0.0-beta.3-windows-amd64.zip',
+            INPUT_NAME: 'helm.exe',
+            INPUT_PATHINARCHIVE: 'windows-amd64/helm.exe'
+        }
+        for (const key in input)
+            process.env[key] = input[key];
+        
+        let c = cfg.getConfig();
+        await c.configure();
+
+        assert.equal(fs.existsSync(path.join(cfg.binPath(), c.name)), true);
+    });
+})
