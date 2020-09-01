17a8e0ec99c875bb7463a2ed36ef97819c4c040b
Radu M <root@radu.sh>
2020-08-31 21:21:52 -0700

    Add initial GitHub related fields
    Signed-off-by: Radu M <root@radu.sh>

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
-import * as core from '@actions/core';
-import * as cfg from './configurator';
+import * as core from "@actions/core";
+import * as cfg from "./configurator";
 
 async function run() {
   try {
     let c = cfg.getConfig();
-    await c.configure();
+    if (c.fromGitHubReleases) {
+      console.log(c);
+      console.log(c.urlTemplate);
+    }
+    //await c.configure();
   } catch (error) {
     core.setFailed(error.message);
   }
